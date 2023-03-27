de53e5a14d8eeb38b50aa20d678508756e549a48
chiadicodewarrior <paulchiadikaobi@gmail.com>
2023-03-27 14:03:53 -0700

    my commit message on alx group project on printf

--- _printf.c
+++ _printf.c
@@ -3,18 +3,18 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf-print function
- * @format:format.
- * Return:printed chars.
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-int_printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-	int i,printned = 0,printed_chars = 0;
-	int flags, width, precision, buff_ind = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
-        if (format ==NULL)
+	if (format == NULL)
 		return (-1);
 
 	va_start(list, format);
@@ -24,36 +24,38 @@ int_printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
-			if( buff_ind == BUFF_SIZE)
+			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write (1, &format[i], 1); */
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
-	        else
+		else
 		{
-                        print_buffer(buffer, &buff_ind);
+			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
-        }
-        print_buffer(buffer, &buff_ind);
+	}
 
-        va_end(list);
+	print_buffer(buffer, &buff_ind);
 
-        return (printed_chars);
+	va_end(list);
+
+	return (printed_chars);
 }
 
 /**
- * print_buffer - prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
- * @buff_ind: index at which to add next char, represents the lenghth.
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
